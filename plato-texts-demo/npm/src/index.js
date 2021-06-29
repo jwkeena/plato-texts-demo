d16800a44f59e891683c7fc2b9371e86817a7e46
jwkeena <jwkeena@gmail.com>
2021-06-29 14:32:55 -0400

    Successful installation of npm packages, including plato-texts. The getPlatoText() function can be called from the browser, using polyfills for node functions that webpack removed in version 5

---
+++ plato-texts-demo/npm/src/index.js
@@ -0,0 +1,3 @@
+﻿import defaultExport from 'plato-texts';
+window.getPlatoText = defaultExport;
+window.Buffer = global.Buffer || require('buffer').Buffer;
---
+++ plato-texts-demo/npm/webpack.config.js
@@ -0,0 +1,34 @@
+﻿const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        // fix "process is not defined" error:
+        // (do "npm install process" before running the build)
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    resolve: {
+        alias: {
+            "https": require.resolve("https-browserify"),
+            "string_decoder": require.resolve("string_decoder/"),
+            "url": require.resolve("url"),
+            "buffer": require.resolve("buffer"),
+            "http": require.resolve("stream-http"),
+            "stream": require.resolve("stream-browserify"),
+            // "process": false, // require.resolve("process/browser")
+        },
+    },
+}
